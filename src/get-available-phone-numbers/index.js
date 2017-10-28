7f18025ca6105b790bd5a24d524dc588d8581df5
timwis <tim@timwis.com>
2017-10-28 19:01:08 -0400

    Change getAvailablePhoneNumbers from a mutation to a query, require login, use users subaccount Closes #9 and closes #7

--- src/get-available-phone-numbers/index.js
+++ src/get-available-phone-numbers/index.js
@@ -1,15 +1,25 @@
 'use latest'
 const twilio = require('twilio')
+const { fromEvent } = require('graphcool-lib')
 const omit = require('lodash/omit')
 const partialRight = require('lodash/partialRight')
 
-const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN } = process.env
 const COUNTRY = 'US'
 const DANGEROUS_FIELDS = ['_version', '_context', '_solution']
 const omitDangerousFields = partialRight(omit, DANGEROUS_FIELDS)
 
 module.exports = async (event) => {
-  const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+  const auth = event.context.auth
+  if (!auth || !auth.nodeId || auth.typeName !== 'User') {
+    return { error: 'Insufficient permissions' }
+  }
+
+  const userId = auth.nodeId
+  const graphcool = fromEvent(event)
+  const api = graphcool.api('simple/v1')
+  const subaccount = await getTwilioSubaccount(userId)
+
+  const client = twilio(subaccount.sid, subaccount.authToken)
 
   const opts = { smsEnabled: true }
   if (event.data.areaCode) opts.areaCode = event.data.areaCode
@@ -21,4 +31,25 @@ module.exports = async (event) => {
 
   const items = response.map(omitDangerousFields)
   return { data: items }
+
+  function getTwilioSubaccount (userId) {
+    const query = `
+      query ($userId: ID!) {
+        allTwilioSubaccounts(filter: {
+          user: {
+            id: $userId
+          }
+        }) {
+          id
+          sid
+          authToken
+          twilioMessagingService {
+            sid
+          }
+        }
+      }
+    `
+    return api.request(query, { userId })
+      .then((response) => response.allTwilioSubaccounts[0])
+  }
 }
